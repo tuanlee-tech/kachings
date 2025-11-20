3991fc737641617db2b6c1f51072d408bc6612c3
tuanlee-tech <tuanlee.tech@gmail.com>
2025-11-21 00:23:58 +0700

    add workflow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/kachings",
   plugins: [react(), tailwindcss(),],
 })
